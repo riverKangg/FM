da45b98c3f4b416f635a9897e8213e0a19802e15
riverKangg <riverKangg@gmail.com>
2023-11-22 06:57:13 +0900

    added dnn_layer.py

---
+++ reco/layers/dnn_layer.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+
+class DNNLayer(nn.Module):
+    def __init__(self, input_dim,
+                 hidden_units=[64, 64, 32],
+                 output_dim=1,
+                 use_bias=True,
+                 dropout_rate=0.0):
+        super(DNNLayer, self).__init__()
+        dnn_layers = []
+        hidden_units = [input_dim] + hidden_units
+        for idx in range(len(hidden_units) - 1):
+            dnn_layers.append(nn.Linear(hidden_units[idx], hidden_units[idx + 1],
+                                        bias=use_bias))
+            dnn_layers.append(nn.BatchNorm1d(hidden_units[idx + 1]))
+            dnn_layers.append(nn.ReLU())
+            dnn_layers.append(nn.Dropout(dropout_rate))
+        dnn_layers.append(nn.Linear(hidden_units[-1], output_dim, bias=use_bias))
+        self.dnn = nn.Sequential(*dnn_layers)
+
+    def forward(self, input):
+        return self.dnn(input)
+
+
+if __name__ == "__main__":
+    input_data = torch.rand((100, 10))
+    dnn = DNNLayer(10)
+    output = dnn(input_data)
+    print(input_data.size(), output.size())
